91146e65be40e45917b1da13df22f28b791a1fb7
Lukasz Stanaszek <lstan2104@gmail.com>
2024-08-16 11:09:19 +0000

    added axios request

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', async(req,res) => {
+    console.log("someone is here");
+    res.json({message: "welcome to weather"});
+});
+
+app.get('/weather', async(req,res) => {
+    console.log("request received");
+
+    const lat = 52.9500;
+    const lon = -1.1500;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`
+
+});
+
 app.listen(port, () => {
     console.log(`Server Active... Running on http://localhost:${port}`);
 });
